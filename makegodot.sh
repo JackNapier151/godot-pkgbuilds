9e0e036afbe660d1b7a1321d6bb8d0fe7d3a52fa
Jack Napier <jacknapier151@gmail.com>
2023-01-26 16:07:48 +0100

    PKGBUILDs Restructured

--- makegodot.sh
+++ makegodot.sh
@@ -7,22 +7,15 @@ srdir='/run/media/odin/338a9a65-34d3-49ef-92f0-76fd53c407ce/AUR/Godot'
 directories=(
 'godot-base' # 1 packages
 
-'godot-dev-bin' # 8 packages
-'godot-alpha-bin' # 9 packages
-'godot-beta-bin' # 21 packages
-'godot-rc-bin' # 21 packages
-'godot-stable-rc-bin' # 20 packages
-'godot-stable-bin' # 20 packages
-
-'godot4-dev-bin' # 8 packages
-'godot4-alpha-bin' # 9 packages
-'godot4-beta-bin' # 21 packages
+'godot4-dev-bin' # 8+8=16 packages
+'godot4-alpha-bin' # 9+9=18 packages
+'godot4-beta-bin' # 21+21=42 packages
 
 'godot3-alpha-bin' # 13 packages
 'godot3-beta-bin' # 21 packages
-'godot3-rc-bin' # 21 packages
-'godot3-stable-rc-bin' # 20 packages
-'godot3-stable-bin' # 20 packages
+'godot3-rc-bin' # 21+21=42 packages
+'godot3-stable-rc-bin' # 20+20=40 packages
+'godot3-stable-bin' # 20+20=40 packages
 
 'godot2-rc-bin' # 8 packages
 'godot2-stable-bin' # 8 packages
@@ -31,7 +24,6 @@ directories=(
 
 #'godot-git' # 1 packages
 )
-
 
 makerepo () {
     ln -s ${srdir}/${1}/*.pkg.tar.zst ${srdir}/godot-repo/
