4bdebf210d01df4d64cc6b50c8cd3ac92e7a3b01
Jack Napier <jacknapier151@gmail.com>
2023-04-01 00:57:01 +0200

    Huge Update: RC and Stable 4.0, Refactoring Mono and Visual, New Godot LTS (godot3), and more

--- makegodot.sh
+++ makegodot.sh
@@ -2,68 +2,73 @@
 # vim: ft=bash fileencoding=utf-8 sts=4 sw=4 et:
 # -*- coding: utf-8 -*-
 
-srdir='/run/media/odin/338a9a65-34d3-49ef-92f0-76fd53c407ce/AUR/Godot'
+srdir='/directorio/en/el/que/estoy/' # Manual
+srdir=$(cat ./makegodot.env) # En Archivo .env (cambiar el nombre quitandole el .bak)
 
 directories=(
 'godot-base' # 1 packages
 
+# 'godot-meta'
+
+# 'godot-git' # 1 packages
+
 'godot4-dev-bin' # 8+8=16 packages
 'godot4-alpha-bin' # 9+9=18 packages
-'godot4-beta-bin' # 21+21=42 packages
-'godot4-rc-bin' # 21+21=42 packages
+'godot4-beta-bin' # 21 packages
+'godot4-rc-bin' # 21 packages
+'godot4-stable-rc-bin' # 21 packages
+'godot4-stable-bin' # 21 packages
 
 'godot3-alpha-bin' # 13 packages
 'godot3-beta-bin' # 21 packages
-'godot3-rc-bin' # 21+21=42 packages
-'godot3-stable-rc-bin' # 20+20=40 packages
-'godot3-stable-bin' # 20+20=40 packages
+'godot3-rc-bin' # 21 packages
+'godot3-stable-rc-bin' # 20 packages
+'godot3-stable-bin' # 20 packages
 
-'godot2-rc-bin' # 8 packages
+'godot2-stable-rc-bin' # 8 packages
 'godot2-stable-bin' # 8 packages
 
 'godot1-stable-bin' # 8 packages
-
-#'godot-git' # 1 packages
 )
 
 makerepo () {
-    ln -s ${srdir}/${1}/*.pkg.tar.zst ${srdir}/godot-repo/
-    ln -s ${srdir}/${1}/*.pkg.tar.zst.sig ${srdir}/godot-repo/
-    repo-add -R -n -p ${srdir}/godot-repo/godot.db.tar.zst ${srdir}/${1}/*.pkg.tar.zst
+	ln -s ${srdir}/${1}/*.pkg.tar.zst ${srdir}/godot-repo/
+	ln -s ${srdir}/${1}/*.pkg.tar.zst.sig ${srdir}/godot-repo/
+	repo-add -R -n -p ${srdir}/godot-repo/godot.db.tar.zst ${srdir}/${1}/*.pkg.tar.zst
 }
 
 rmbrokenlinks (){
-    for link in $(find "${srdir}/godot-repo/" -xtype l);do
-        echo "$(tput setaf 1) $link $(tput sgr 0)"
-        rm -f $link
-    done
+	for link in $(find "${srdir}/godot-repo/" -xtype l);do
+		echo "$(tput setaf 1) $link $(tput sgr 0)"
+		rm -f $link
+	done
 }
 
 makeapp () {
-    cd "${1}"
-    makepkg -c --sign --allsource
-    makepkg -c --sign
-    makerepo ${1}
-    cd ..
+	makepkg -c --sign --allsource
+	makepkg -c --sign
 }
 
 gitupdate (){
-    eval $(ssh-agent -s)
-    ssh-add /home/odin/.ssh/jacknapier151gmail
-    git add .
-    read -p "Commit: " input
-    git commit -m "${input}"
-    git push -u origin master
-    pkill ssh-agent
+	eval $(ssh-agent -s)
+	ssh-add /home/odin/.ssh/jacknapier151gmail
+	git add .
+	read -p "Commit: " input
+	git commit -m "${input}"
+	git push -u origin master
+	pkill ssh-agent
 }
 
 main (){
-    for makedir in "${directories[@]}"  
-    do
-        makeapp ${makedir}
-    done
-    rmbrokenlinks
-    gitupdate
+	for makedir in "${directories[@]}"  
+	do
+		cd "${makedir}"
+		makeapp
+		makerepo ${makedir}
+		cd ..
+	done
+	rmbrokenlinks
+	gitupdate
 }
 
 main
--- makerepogodot.sh
+++ makerepogodot.sh
@@ -2,21 +2,22 @@
 # vim: ft=bash fileencoding=utf-8 sts=4 sw=4 et:
 # -*- coding: utf-8 -*-
 
-srdir='/run/media/odin/338a9a65-34d3-49ef-92f0-76fd53c407ce/AUR/Godot'
+srdir='/directorio/en/el/que/estoy/' # Manual
+srdir=$(cat ./makegodot.env) # En Archivo .env (cambiar el nombre quitandole el .bak)
 
 directories=(
 'godot-base' # 1 packages
 
-'godot-dev-bin' # 8 packages
-'godot-alpha-bin' # 9 packages
-'godot-beta-bin' # 21 packages
-'godot-rc-bin' # 21 packages
-'godot-stable-rc-bin' # 20 packages
-'godot-stable-bin' # 20 packages
+# 'godot-meta'
 
-'godot4-dev-bin' # 8 packages
-'godot4-alpha-bin' # 9 packages
+# 'godot-git' # 1 packages
+
+'godot4-dev-bin' # 8+8=16 packages
+'godot4-alpha-bin' # 9+9=18 packages
 'godot4-beta-bin' # 21 packages
+'godot4-rc-bin' # 21 packages
+'godot4-stable-rc-bin' # 21 packages
+'godot4-stable-bin' # 21 packages
 
 'godot3-alpha-bin' # 13 packages
 'godot3-beta-bin' # 21 packages
@@ -24,27 +25,25 @@ directories=(
 'godot3-stable-rc-bin' # 20 packages
 'godot3-stable-bin' # 20 packages
 
-'godot2-rc-bin' # 8 packages
+'godot2-stable-rc-bin' # 8 packages
 'godot2-stable-bin' # 8 packages
 
 'godot1-stable-bin' # 8 packages
-
-#'godot-git' # 1 packages
 )
 
-makeapp () {
-    cd "${1}"
-    repo-add -R -n -p ${srdir}/godot-repo/godot.db.tar.zst ${srdir}/${1}/*.pkg.tar.zst
-    ln -s ${srdir}/${1}/*.pkg.tar.zst ${srdir}/godot-repo/
-    ln -s ${srdir}/${1}/*.pkg.tar.zst.sig ${srdir}/godot-repo/
-    cd ..
+makerepo () {
+	ln -s ${srdir}/${1}/*.pkg.tar.zst ${srdir}/godot-repo/
+	ln -s ${srdir}/${1}/*.pkg.tar.zst.sig ${srdir}/godot-repo/
+	repo-add -R -n -p ${srdir}/godot-repo/godot.db.tar.zst ${srdir}/${1}/*.pkg.tar.zst
 }
 
 main (){
-    for makedir in "${directories[@]}" 
-    do
-        makeapp ${makedir}
-    done
+	for makedir in "${directories[@]}" 
+	do
+		cd ${makedir}
+		makerepo ${makedir}
+		cd ..
+	done
 }
 
 main
